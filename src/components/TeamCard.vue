2709770b7ef89bbb1c3207407c60fcf3362a9c40
hisc94 <haroldisaias20@hotmail.com>
2020-12-05 20:31:52 -0500

    componente TeamCard

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
 
+    <div class="card text-white bg-dark" id="box-team">
+        <div class="d-flex justify-content-center p-2">
+            <img :src="member.imagen" alt="fotografia equipo" id="img_team">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Team Member: {{member.codigo}}</h5>
+            <p class="card-text">{{member.nombre}}</p>
+            <p class="card-text">{{member.rol}}</p>
+            <p class="card-text">{{member.descripcion}}</p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
     </div>
 </template>
 
@@ -12,5 +24,13 @@
 </script>
 
 <style scoped>
+    #img_team{
+        width:200px;
+        height:200px;
+    }
 
+    #box-team{
+        width:445px;
+        height:550px;
+    }
 </style>
